9072c4b392260e6cd0afbb143e0b44ca8f4be5e1
nobabsarkar <nobabsarkar2016@gmail.com>
2024-05-21 23:08:00 +0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  }
+);
